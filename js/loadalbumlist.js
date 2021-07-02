1f75b034025d36497ff455f05dfa79d6cd8e4e69
sepintar360 <61118412+sepintar360@users.noreply.github.com>
2021-07-02 17:29:23 +0700

    Add files via upload

--- js/loadalbumlist.js
+++ js/loadalbumlist.js
@@ -1,11 +1,2 @@
-
-$(document).ready(function(){
-$('head').append("<title>ALBUM - "+decodeURIComponent(okehash[2])+"</title>");
-var textpret = "";
-for (i = 0; i < dataalbumlist.length; i++) { 
-if(dataalbumlist[i].id_band == okehash[1]) {
-    textpret += "<a id=\""+ dataalbumlist[i].id +"\" href=\"chord.html#"+ dataalbumlist[i].id +"\"><h4>"+ dataalbumlist[i].judul + "</h4></a>";
-}};
-document.getElementById("hasillist").innerHTML = textpret;
-});
-var message='Album - '+decodeURIComponent(okehash[2])+'';
+$(document).ready(function(){$('#title').append("ALBUM - "+decodeURIComponent(okehash[2]));var textpret="";for(i=0;i<dataalbumlist.length;i++){if(dataalbumlist[i].id_band==okehash[1]){textpret+="<a id=\""+dataalbumlist[i].id+"\" href=\"chord.html#"+dataalbumlist[i].id+"\"><h4>"+dataalbumlist[i].judul+"</h4></a>";}};document.getElementById("hasillist").innerHTML=textpret;});var message='Album - '+decodeURIComponent(okehash[2])+'';
+okehash[2])+'';
